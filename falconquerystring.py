1b075caa20031fe85eebcbf8cbaa43c2ef407cc5
Abhiramianilkumar <73107802+Abhiramianilkumar@users.noreply.github.com>
2021-04-01 20:54:46 +0530

    falcon querystring

--- falconquerystring.py
+++ falconquerystring.py
@@ -6,8 +6,7 @@ class AboutResource(object):
         n1=req.get_param('no1')
         n2=req.get_param('no2')
         answer=int(n1)+int(n2)
-        about = {
             'Author':answer
         }
         
-        resp.body = json.dumps(about)
+        resp.body = json.dumps(add)
